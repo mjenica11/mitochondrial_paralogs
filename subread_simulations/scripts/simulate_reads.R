e681d062964ac31e767596342d8bba1e50e99df9
Mollie Peters <mjpete11@asu.edu>
2022-01-26 16:16:28 -0700

    different function to simulate reads

---
+++ subread_simulations/scripts/simulate_reads.R
@@ -0,0 +1,18 @@
+library(Rsubread)
+
+simReads(# transcript database and wanted abundance
+		 transcript.file="/scratch/mjpete11/subread_simulations/fastas/SLC25A4.fa", 
+		 expression.levels=100, 
+		 # the name of the output
+		 output.prefix="SLC25A4", 
+		 # options on the output
+		 library.size=100, read.length=100, truth.in.read.names=TRUE, 
+		 # simulating sequencing errors
+		 simulate.sequencing.error=TRUE, quality.reference=NULL,
+		 # parameters for generating paired-end reads
+		 paired.end=TRUE, 
+		 fragment.length.min=100,
+		 fragment.length.max=500,
+		 fragment.length.mean=180,
+		 fragment.length.sd=40)
+
