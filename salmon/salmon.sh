2ade9ab634f36475eb87a8a1d2844f7932d4eb92
Mollie Peters <mjpete11@asu.edu>
2022-02-03 15:35:14 -0700

    salmon with GRCh38

--- salmon/salmon.sh
+++ salmon/salmon.sh
@@ -7,23 +7,12 @@ module purge
 source /home/mjpete11/.bashrc
 conda activate salmon_160
 
-SALMON_INDEX=/scratch/mjpete11/salmon/default
+SALMON_INDEX=/scratch/mjpete11/salmon/index/GRCh38_index/
 LIBTYPE=A
 THREADS=8
 
-fq1=/scratch/mjpete11/subread_simulations/scripts/output/combined_R1.fastq
-fq2=/scratch/mjpete11/subread_simulations/scripts/output/combined_R2.fastq
-OUTPUT=mapped_reads/combined.fasta
+fq1=/scratch/mjpete11/subread_simulations/scripts/single_transcript/100_reads_SLC25A4_R1.fastq
+fq2=/scratch/mjpete11/subread_simulations/scripts/single_transcript/100_reads_SLC25A4_R2.fastq
+OUTPUT=mapped_reads/SLC25A4.fasta
 
 salmon quant -i $SALMON_INDEX -p $THREADS -l $LIBTYPE -1 $fq1 -2 $fq2 -o $OUTPUT
-
-# Code to quantify n samples
-#for i in {1..1000}
-#do
-#   count=$(printf "%02d" $i)
-#	fq1=/scratch/mjpete11/subread_simulations/scripts/sample_${count}_R1.fasta
-#	fq1=/scratch/mjpete11/subread_simulations/scripts/sample_${count}_R2.fasta
-#	OUTPUT=mapped_reads/$sample_{count}.fasta 
-    
-#	salmon quant -i $SALMON_INDEX -p $threads -l $LIBTYPE -1 $fq1 -2 $fq2 -o $OUTPUT
-#done
