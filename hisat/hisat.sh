dad74732d95c70505bfe9cb49fee43853fd71fd9
Mollie Peters <mjpete11@asu.edu>
2022-02-07 16:55:57 -0700

    sorted BAM file and created index

--- hisat/hisat.sh
+++ hisat/hisat.sh
@@ -9,8 +9,8 @@ conda activate hisat2
 
 fq1=/scratch/mjpete11/subread_simulations/scripts/single_transcript/100_reads_SLC25A4_R1.fastq
 fq2=/scratch/mjpete11/subread_simulations/scripts/single_transcript/100_reads_SLC25A4_R2.fastq
-#HISAT2_INDEXES=/scratch/mjpete11/hisat/index
 BASENAME=GRCh38
-SAM=/scratch/mjpete11/hisat/hisat_alignment.sam
+BAM=hisat_alignment.bam
 
-hisat2 -q -x $BASENAME -1 $fq1 -2 $fq2 | samtools view -o $SAM
+hisat2 -q -x $BASENAME -1 $fq1 -2 $fq2 | samtools sort -o $BAM 
+samtools index $BAM 
