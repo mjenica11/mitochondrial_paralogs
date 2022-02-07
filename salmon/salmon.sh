1bcb9fa2e3712ca8410d1ff9ac30b87db9e4a661
Mollie Peters <mjpete11@asu.edu>
2022-02-07 14:52:44 -0700

    quantify 100 reads from 1 transcript with salmon using GRCh38 transcriptome

--- salmon/salmon.sh
+++ salmon/salmon.sh
@@ -13,6 +13,6 @@ THREADS=8
 
 fq1=/scratch/mjpete11/subread_simulations/scripts/single_transcript/100_reads_SLC25A4_R1.fastq
 fq2=/scratch/mjpete11/subread_simulations/scripts/single_transcript/100_reads_SLC25A4_R2.fastq
-OUTPUT=mapped_reads/SLC25A4.fasta
+OUTPUT=test_mapped_reads/100_reads_SLC25A4.fasta
 
 salmon quant -i $SALMON_INDEX -p $THREADS -l $LIBTYPE -1 $fq1 -2 $fq2 -o $OUTPUT
