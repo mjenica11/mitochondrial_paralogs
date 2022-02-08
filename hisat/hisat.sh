8ee34d5fa5e13fc4d03efdf83678b23d203de9a9
Mollie Peters <mjpete11@asu.edu>
2022-02-07 17:59:48 -0700

    count the number of reads per gene

--- hisat/hisat.sh
+++ hisat/hisat.sh
@@ -11,6 +11,18 @@ fq1=/scratch/mjpete11/subread_simulations/scripts/single_transcript/100_reads_SL
 fq2=/scratch/mjpete11/subread_simulations/scripts/single_transcript/100_reads_SLC25A4_R2.fastq
 BASENAME=GRCh38
 BAM=hisat_alignment.bam
+GFF=GRCh38_latest_genomic.gff
+GTF=GRCh38_latest_genomic.gtf
+NUMREADS=number_reads.count
 
+# Align and sort reads
 hisat2 -q -x $BASENAME -1 $fq1 -2 $fq2 | samtools sort -o $BAM 
+
+# Make index
 samtools index $BAM 
+
+# Convert GFF file to GTF
+gffread $GFF -T -o $GTF
+
+# Count mapped reads per transcript
+htseq-count -r pos -t CDS -f bam $BAM $GTF > $NUMREADS
