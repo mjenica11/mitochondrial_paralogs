644b9caad8eb417462256435d4b7c0ae7dc3e4ac
Mollie Peters <mjpete11@asu.edu>
2022-02-14 17:53:46 -0700

    reference genome

---
+++ hisat/build_index.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -n 8
+#SBATCH -o sbatch_test.out
+#SBATCH -p htc
+
+# Manually downloaded RefSeq transcripts from:
+# https://www.ncbi.nlm.nih.gov/genome/guide/human/
+
+# Build HGFM index
+hisat2-build -p 16 GRCh38_latest_genomic.fna GRCh38
