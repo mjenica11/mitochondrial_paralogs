e87abb013884790dc0c08de549aec382ef16f659
Mollie Peters <mjpete11@asu.edu>
2022-03-07 16:57:35 -0700

    table of true positives from individual simualtions

--- salmon/num_reads_table.R
+++ salmon/num_reads_table.R
@@ -1,12 +1,16 @@
 library(data.table)
 
-# Read in the salmon quant.sf file: contains the number of reads mapped
-# to each transcript in the reference transcriptome
-dat <- read.csv("SLC25A4_1000_mapped_reads/quant.sf", sep="\t")
+# Read in the quant.sf files
+files <- list.files("individual_simulations", pattern="*.sf", recursive=TRUE, full.names=TRUE)
+res <- lapply(files, function(i){
+					  read.csv(i, header=TRUE, sep='\t')
+})
 
 # Rename the "Name" column so it can be merged with another dataframe
-colnames(dat)[1] <- "RefSeq_ID"
-head(dat)
+colnames <- c("RefSeq_ID", "Length", "EffectiveLength", "TPM", "NumReads")
+for (i in seq_along(res)){
+		colnames(res[[i]]) <- colnames 
+}
 
 # Subset the SLC25 transcripts from the quant.sf file
 # Chose the transcripts that are present in both RefSeq and Ensembl as the
@@ -28,8 +32,8 @@ RefSeq <-c("NM_032315.3", "NM_207348.3", "NM_013386.5", "NM_014655.4",
 		   "NM_001282195.2")
 
 # Dataframe of just SLC25 read counts
-dat2 <- subset(dat, dat$Name %in% RefSeq)
-nrow(dat2) # 53
+dat <- lapply(res, function(x) subset(x, x$RefSeq_ID %in% RefSeq))
+lapply(dat, nrow)
 
 # Append column with the HGNC names of the SLC25 transcripts for readability
 SLC25_genes  <- c("SLC25A33", "SLC25A34", "SLC25A24", "SLC25A44", "SLC25A12",
@@ -48,35 +52,51 @@ SLC25_genes  <- c("SLC25A33", "SLC25A34", "SLC25A24", "SLC25A44", "SLC25A12",
 # Add a column with the HGNC gene names
 temp <- data.frame(HGNC_names = SLC25_genes, RefSeq_ID = RefSeq) 
 
-# Bind the name dataframe to the quant.sf dataframe and merge by "Name"
+# Bind the name dataframe to the quant.sf dataframe and merge by "RefSeq_ID"
 # to make sure the rows bind in the same order
 # Manually checked to make sure the RefSeq IDs matched the HGNC IDs
-dat2 <- merge(dat, temp, by = "RefSeq_ID")
-head(dat2) 
+dat2 <- lapply(dat, function(x) merge(x, temp, by="RefSeq_ID"))
+head(dat2[[1]]);head(dat2[[53]])
 
-# Move the HGNC_names colum to the begining
-dat2 <- dat2[,c(6,1,2,3,5,4)]
-head(dat2)
+# Move the HGNC_names column to the begining
+dat2 <- lapply(dat2, function(x) x[,c(6,1,2,3,5,4)])
 
 # The number of reads simulated per transcript
-# Instantiate vector of 0s equal to the number of SLC25 family members present
-expected <- rep(0, nrow(dat2))
-
-# Insert 1000 at the index for the transcript that the reads were  simulated from (SLC25A4) 
-which(dat2$RefSeq_ID %in% "NM_001151.4") #9
-expected[9] <- 1000
+expected <- rep(1000, nrow(dat2[[1]]))
 expected
+dat3 <- lapply(dat2, function(x) cbind(x, simulated_fragments = expected))
 
-# Add a column with the number of simulated transcripts
-dat2$simulated_fragments <- expected
-dat2[9:20,]
+# Combine each df into one
+genes <- c("SLC25A1","SLC25A10","SLC25A11","SLC25A12","SLC25A13","SLC25A14",
+		  "SLC25A15","SLC25A16","SLC25A17","SLC25A18","SLC25A19","SLC25A2",
+		  "SLC25A20","SLC25A21","SLC25A22","SLC25A23","SLC25A24","SLC25A25",
+		  "SLC25A26","SLC25A27","SLC25A28","SLC25A29","SLC25A3","SLC25A30",
+		  "SLC25A31","SLC25A32","SLC25A33","SLC25A34","SLC25A35","SLC25A36",
+		  "SLC25A37","SLC25A38","SLC25A39","SLC25A4","SLC25A40","SLC25A41",
+		  "SLC25A42","SLC25A43","SLC25A44","SLC25A45","SLC25A46","SLC25A47",
+		  "SLC25A48","SLC25A49","SLC25A5","SLC25A50","SLC25A51","SLC25A52",
+		  "SLC25A53", "SLC25A6", "SLC25A7", "SLC25A8", "SLC25A9")
+
+# Function to slice relevant row from each df in list of dataframes
+slice_dataframes <- function(vec, name){
+		dx <- dat3[[vec]][which(dat3[[vec]]$HGNC_names %in% name),]
+		return(dx)
+}
+
+# Apply function to list of dfs
+df_lst <- Map(slice_dataframes, vec = seq(1:53), name = genes)
+df_lst
+
+# Merge dataframes
+cols <- c("HGNC_names", "RefSeq_ID", "Length", "EffectiveLength", "NumReads", "TPM", "simulated_fragments")
+dat4 <- Reduce(function(...) merge(..., all = TRUE), df_lst)
+head(dat4)
+tail(dat4)
+
+# Add a column with the true positive rate
+dat4$true_positive <- dat4$NumReads/dat4$simulated_fragments * 100
+min(dat4$true_positive) 
+max(dat4$true_positive) 
 
 # Write to file
-#write.csv(dat2, "output/1000_reads_SLC25A4_transcripts.csv")
-
-# Confusion matrix
-predicted <- as.factor(dat2$simulated_fragments)
-observed <- as.factor(dat2$NumReads)
-tab <- table(predicted, observed)
-prop.table(predicted, observed)
-write.csv(tab, "output/test_table.csv")
+write.csv(dat4, "output/1000_reads_individual_transcripts.csv")
