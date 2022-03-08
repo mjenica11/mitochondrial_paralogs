014af4074a577c5978ac117b73165e2fc47f3e6d
Mollie Peters <mjpete11@asu.edu>
2022-03-07 21:43:38 -0700

    quantify all individual simulations

--- salmon/salmon.sh
+++ salmon/salmon.sh
@@ -11,8 +11,15 @@ SALMON_INDEX=/scratch/mjpete11/salmon/index/GRCh38_index/
 LIBTYPE=A
 THREADS=8
 
-fq1=/scratch/mjpete11/subread_simulations/scripts/single_transcript/100_reads_SLC25A4_R1.fastq
-fq2=/scratch/mjpete11/subread_simulations/scripts/single_transcript/100_reads_SLC25A4_R2.fastq
-OUTPUT=test_mapped_reads/100_reads_SLC25A4.fasta
+for i in {1..53}
+do
+		count=$(printf "%02d" $i)
+		fq1=/scratch/mjpete11/subread_simulations/scripts/individual_simulations/SLC25A${count}_R1.fastq
+		fq2=/scratch/mjpete11/subread_simulations/scripts/individual_simulations/SLC25A${count}_R2.fastq
+		OUTPUT=individual_simulations/SLC25A${count}
+		MAPPING=individual_simulations/SLC25A${count}/mapping_info.csv
 
-salmon quant -i $SALMON_INDEX -p $THREADS -l $LIBTYPE -1 $fq1 -2 $fq2 -o $OUTPUT
+		salmon quant -i $SALMON_INDEX -p $THREADS -l $LIBTYPE -1 $fq1 -2 $fq2 \
+			--dumpEq --validateMappings --writeMappings $MAPPING \
+			--writeUnmappedNames -o $OUTPUT
+done
