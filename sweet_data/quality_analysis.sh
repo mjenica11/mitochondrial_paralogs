c95da424d4c3afe098986e513ada28a10e4a4ed4
Mollie Peters <mjpete11@asu.edu>
2022-04-04 15:50:30 -0700

    fastqc and multiqc reports

---
+++ sweet_data/quality_analysis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -n 8
+#SBATCH -o sbatch_test.out
+#SBATCH -p htc
+
+module purge
+source /home/mjpete11/.bashrc
+conda activate fastqc_environment
+
+for i in {784..847}
+do
+	count=$(printf $i)
+	fastqc -o fastqc_results SRR7426${count}.fastq
+done
