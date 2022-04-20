b282c3d249a0a2a99d2cba383afa10ce580dbf85
Mollie Peters <mjpete11@asu.edu>
2022-04-20 12:14:10 -0700

    added multiqc step

--- GTEx/liver/slc25_multiqc/subset_paralogs.sh
+++ GTEx/liver/slc25_multiqc/subset_paralogs.sh
@@ -1,24 +1,32 @@
 #!/bin/bash 
 #SBATCH -o sbatch.out
-#SBATCH -p phi
+#SBATCH -p htc 
 
 module purge
 source /home/mjpete11/.bashrc
-conda activate samtools 
-
-# List of GTEx sample IDs
-array=(11DXY-0526-SM-5EGGQ 11EQ9-0526-SM-5A5JZ 11DXZ-0126-SM-5EGGY)
+conda activate data_science 
 
 # Path
-PATH=/data/CEM/shared/controlled_access/GTEX/RNA/liver/bam
+GTEX_PATH=/data/CEM/shared/controlled_access/GTEX/RNA/liver/bam
 FILEEND=Aligned.sortedByCoord.out.patched.md
 
 # Define a list
-for item in ${!array[@]}; do
-	samtools view -h -b -L slc25_genes.bed $PATH/GTEX-${array[$item]}.$FILEEND.bam /
-	>> slc25_reads/GTEX-${array[$item]}_mapped.bam
+BAMS=($(readlink -f $GTEX_PATH/*bam))
+item=${BAMS[0]}
+
+for item in ${BAMS[@]}
+do
+    outname=$(basename $item)
+    outname=slc25_reads/${outname%.bam}_mapped.bam
+    samtools view -h -L slc25_genes.bed $item > $outname
 done
 
-#for i in ${!array}; do
-#		fastqc -o fastqc_results mapped_reads/GTEX-${array[$index]}_mapped.bam
-#done
+# FASTQC + MUTLIQC on the BAM files subset to just reads that map to slc25 genes
+SUBSET_BAMS=($(readlink -f /scratch/mjpete11/GTEx/liver/slc25_multiqc/slc25_reads/*bam))
+
+for file in ${SUBSET_BAMS[@]}
+do
+	fastqc -o fastqc_results $file 
+done
+
+multiqc .
