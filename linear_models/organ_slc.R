bbfadf0afdb905118eb5394ad2ceeeedf45210c8
Mollie Peters <mjpete11@asu.edu>
2022-08-06 19:45:41 -0700

    started slc ~ organ

---
+++ linear_models/organ_slc.R
@@ -0,0 +1,54 @@
+# Purpose: Linear models to asses if SLC paralogs are dependent on energetic state?
+
+# Libraries
+library(limma)
+library(data.table)
+
+# Read in GTEx manifest
+manifest <- read.csv("sample.tsv", header=TRUE, sep = "\t")
+
+# Make dataframe with sample id, tissue type
+# All of the rin number and ischemic time values were missing...
+df1 <- data.frame(manifest$"dbgap_sample_id", manifest$"tissue_type")
+
+# Remove all rows with NA in either columns
+df2 <- df1[complete.cases(df1), ]
+
+# Read in GTEx counts
+counts <- fread("GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct", sep="\t")
+
+# List of SLC25 paralogs
+SLC <- c("SLC25A1", "SLC25A2", "SLC25A3", "SLC25A4", "SLC25A5", "SLC25A6", 
+		 "SLC25A7", "SLC25A8", "SLC25A9", "SLC25A10", "SLC25A11", "SLC25A12", 
+		 "SLC25A13", "SLC25A14", "SLC25A15", "SLC25A16", "SLC25A17", 
+		 "SLC25A18", "SLC25A19", "SLC25A20", "SLC25A21", "SLC25A22", 
+		 "SLC25A23", "SLC25A24", "SLC25A25", "SLC25A26", "SLC25A27", 
+		 "SLC25A28", "SLC25A29", "SLC25A30", "SLC25A31", "SLC25A32", 
+		 "SLC25A33", "SLC25A34", "SLC25A35", "SLC25A36", "SLC25A37", 
+		 "SLC25A38", "SLC25A39", "SLC25A40", "SLC25A41", "SLC25A42", 
+		 "SLC25A43", "SLC25A44", "SLC25A45", "SLC25A46", "SLC25A47",
+		 "SLC25A48", "SLC25A49", "SLC25A50", "SLC25A51", "SLC25A52", "SLC25A53")
+
+# Subset the SLC25 genes
+sub_df <- counts[counts$"Description" %in% SLC, ]
+
+# Only 49 genes are present; which are missing?
+setdiff(SLC, sub_df$'Description') 
+# "SLC25A7"  "SLC25A8"  "SLC25A9"  "SLC25A49" "SLC25A50"
+# I searched for the ENSG ID but I couldn't find them
+
+# Make a df of the sample, gene name, counts, and organ
+head(df2)
+sub_df[1:5,1:5]
+
+# Read in sample attributes files
+file2 <- read.csv("GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt", sep="\t")
+
+# Subset heart left ventricl and liver samples
+vec <- c("Heart - Left Ventricle", "Liver")
+file3 <- file2[file2$SMTSD %in% vec, ]
+
+# Subset the SLC25 gene count df by the sample IDs that match the IDs in file3 df
+df3 <- sub_df[,file3$SAMPID]
+
+
