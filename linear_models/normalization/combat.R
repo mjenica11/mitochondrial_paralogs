89a0c728fedefa5bfbd1cb633ce3b3f27b755851
Mollie Peters <mjpete11@asu.edu>
2022-10-24 18:26:47 -0700

    filtered log10 counts

--- linear_models/normalization/combat.R
+++ linear_models/normalization/combat.R
@@ -21,47 +21,57 @@ samples <- manifest$SAMPID
 # Only keep samples that are present in the manifest 
 counts2 <- counts[,..samples]
 
-##### TEST #####
-#counts2 <- counts2[1:10,1:10] 
+##### TEST ####
+#counts2 <- counts2[1:10,1:10]
+##### TEST ####
 
 # Replace 0s with 1s 
 counts2[counts2 == 0] <- 1
 
 # Log transform the counts but skip the first column
-counts2 <- log10(as.matrix(counts2))
+counts2 <- as.data.frame(log10(as.matrix(counts2)))
 
 # Check if any negative values
 any(counts2)<0 # FALSE
 
+##### TEST ####
+#counts2$"Description" <- c("Hello","Hello","Hello","Hello","Hello","Hello","Hello","Hello","Hello","Hello")
+##### TEST ####
+
+# Add gene names column back on
+counts2$"Description" <- counts$"Description"
+
+# Move the gene names column to the front
+counts2 <- counts2 %>% select("Description", everything())
+
 # Median filter: Function to filter rows that do not have a median >= condition
 median_filter <- function(DF, thresh){
-				DAT <- DF[which(apply(DF,1,median) > thresh), ]
+				DAT <- DF[which(apply(DF[,-c(1)],1,median) > thresh), ]
 				return(DAT)
 }
 
 # Filter rows with a median of < 0 log(counts)
 counts3 <- median_filter(DF=counts2, thresh=0)
 
+# Drop samples from manifest that are missing in the count data
+mani <- manifest[(manifest$SAMPID %in% colnames(counts3)[2:ncol(counts3)]),]
+
 # Make a list of known batch variables; genotype or expression batch ID 
-batch <- manifest$SMGEBTCH
-#batch <- manifest$SMGEBTCH[1:10]
+batch <- mani$SMGEBTCH
 
 # Create a model matrix for the adjustment variables
 # Only added and intercept term, since I am only adjusting for batch
-modcombat <- model.matrix(~1, data=manifest)
-#modcombat <- model.matrix(~1, data=manifest[1:10,])
+modcombat <- model.matrix(~1, data=mani)
 
 # Apply ComBat to the data, using parametric empirical Bayesian adjustment
-combat_edata <- ComBat(dat=counts2, batch=batch, 
-					   mod=modcombat, par.prior=TRUE)
+combat_edata <- ComBat(dat=counts3[,-c(1)], batch=batch, mod=modcombat, par.prior=TRUE)
 
 # Append gene names back 
-combat <- as.data.frame(combat_edata)
-#combat$"Description" <- counts$"Description"[1:10]
-combat$"Description" <- counts$"Description"
+combat_edata <- as.data.frame(combat_edata)
+combat_edata$"Description" <- counts3$"Description"
 
 # Move the gene names column to the front
-combat <- combat %>% select("Description", everything())
+combat_edata <- combat_edata %>% select("Description", everything())
 
 # Write to file
 write.csv(combat_edata, "/scratch/mjpete11/linear_models/data/combat_batch_adjusted_counts_zero_filtered.csv")
