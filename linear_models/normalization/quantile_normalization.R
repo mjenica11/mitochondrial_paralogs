7ba0f527fbe2b0b41117e48d3ac5ac0919e66268
Mollie Peters <mjpete11@asu.edu>
2022-10-24 18:26:56 -0700

    filtered log10 counts

--- linear_models/normalization/quantile_normalization.R
+++ linear_models/normalization/quantile_normalization.R
@@ -12,20 +12,27 @@ library(sva)
 # Read in GTEx manifest
 manifest <- read.csv("/scratch/mjpete11/linear_models/data/sample.tsv", header=TRUE, sep = "\t")
 
-# Make dataframe with sample id, tissue type
-# All of the rin number and ischemic time values were missing...
-df1 <- data.frame(manifest$"dbgap_sample_id", manifest$"tissue_type")
-
-# Remove all rows with NA in either columns
-df2 <- df1[complete.cases(df1), ]
-
 # Read in GTEx counts
-counts <- fread("/scratch/mjpete11/linear_models/data/combat_batch_adjusted_counts.csv", sep=",")
+counts <- fread("/scratch/mjpete11/linear_models/data/combat_batch_adjusted_counts_zero_filtered.csv", sep=",")
+
+SLC <- c("SLC25A1", "SLC25A2", "SLC25A3", "SLC25A4", "SLC25A5", "SLC25A6",
+		 "UCP1", "UCP2", "UCP3", "SLC25A10", "SLC25A11", "SLC25A12", 
+		 "SLC25A13", "SLC25A14", "SLC25A15", "SLC25A16", "SLC25A17", 
+		 "SLC25A18", "SLC25A19", "SLC25A20", "SLC25A21", "SLC25A22", 
+		 "SLC25A23", "SLC25A24", "SLC25A25", "SLC25A26", "SLC25A27", 
+		 "SLC25A28", "SLC25A29", "SLC25A30", "SLC25A31", "SLC25A32", 
+		 "SLC25A33", "SLC25A34", "SLC25A35", "SLC25A36", "SLC25A37", 
+		 "SLC25A38", "SLC25A39", "SLC25A40", "SLC25A41", "SLC25A42", 
+		 "SLC25A43", "SLC25A44", "SLC25A45", "SLC25A46", "SLC25A47",
+		 "SLC25A48", "MTCH1", "MTCH2", "SLC25A51", "SLC25A52", "SLC25A53")
+
+# Are any genes missing?
+setdiff(SLC, counts$'Description') 
 
 #_______________________________________________________________________________ 
 # Perform quantile normalization 
 #_______________________________________________________________________________ 
-rownames(counts) <- counts$Name
+rownames(counts) <- counts$V1
 
 quantile_normalisation <- function(df){
 		 # Determine the ranks of each column from lowest to highest
@@ -44,10 +51,15 @@ quantile_normalisation <- function(df){
 	  return(df_final)
 }
 
-#new_data <- quantile_normalisation(counts[,2:ncol(counts)])
-#new_data <- counts[1:5,3:7]
 new_data <- quantile_normalisation(counts[,3:ncol(counts)])
 new_data[1:5,1:5]
 
+# Add gene names column back
+new_data <- as.data.frame(new_data)
+new_data$"Description" <- counts$"Description"
+
+# Move the gene names column to the front
+new_data <- new_data %>% select("Description", everything())
+
 # Write to file
-write.csv(new_data, "/scratch/mjpete11/linear_models/data/quantile_normalized_counts.csv")
+write.csv(new_data, "/scratch/mjpete11/linear_models/data/quantile_normalized_counts_zero_filtered.csv")
