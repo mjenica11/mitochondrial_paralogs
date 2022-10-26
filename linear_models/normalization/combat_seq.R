f4895e5701a200effd1a3a10c5f7b03951b4a233
Mollie Peters <mjpete11@asu.edu>
2022-10-26 15:20:38 -0700

    neg binom model uses raw counts

--- linear_models/normalization/combat_seq.R
+++ linear_models/normalization/combat_seq.R
@@ -17,18 +17,9 @@ manifest <- read.csv("/scratch/mjpete11/linear_models/data/GTEx_Analysis_v8_Anno
 rownames(manifest) <- manifest$SAMPID
 manifest <- manifest[(manifest$SAMPID %in% colnames(counts)[3:ncol(counts)]),]
 
-# Remove samples that were sequenced "alone"
 # ComBat-seq does not support 1 sample per batch
-#sequenced_alone <- as.list(unique(manifest$SMGEBTCH)) # 566 samples were sequenced alone
-#mani <- manifest[!manifest$SMGEBTCH %in% sequenced_alone, ] # drop all rows, not sure why
-#mani <- manifest[-match(sequenced_alone, manifest$SMGEBTCH),]
-
 # Remove samples (rows) that were sequenced alone from the manifest
 mani <- manifest[manifest$SMGEBTCH %in% manifest$SMGEBTCH[duplicated(manifest$SMGEBTCH)],]
-
-# Are there any unique batch variable IDs?
-length(unique(mani$SMGEBTCH)) # Hmm...336 unique batch IDs left...
-length(duplicated(mani$SMGEBTCH)) # But this says there are nrow(mani) duplicated batch IDs?
 
 # Only keep samples that are present in the manifest which were sequenced 
 # with at least one other sample
@@ -41,16 +32,18 @@ counts2$"Description" <- counts$"Description"
 # Move the gene names column to the front
 counts2 <- counts2 %>% select("Description", everything())
 
-# Median filter: Function to filter rows that do not have a median >= condition
-# chose a higher threshold because you are not supposed to log transform with ComBat-seq
+# Median filter: Drop a gene if no individual has <5 counts
 median_filter <- function(DF, thresh){
-				DAT <- DF[which(apply(DF[,-c(1)],1,median) > thresh), ]
+				DAT <- DF[!rowSums(DF<5), ]
 				return(DAT)
 }
 
 # Filter rows with a median of < 0 log(counts)
 # Skip first column when applying function since it contains the gene names
-counts3 <- median_filter(DF=counts2, thresh=10)
+counts3 <- median_filter(DF=counts2, thresh=5)
+
+# How many genes are left?
+nrow(counts3)
 
 # None of the batch IDs should be unique
 length(unique(mani$SMGEBTCH))
@@ -63,9 +56,7 @@ batch <- mani$SMGEBTCH
 modcombat <- model.matrix(~1, data=mani)
 
 # Apply ComBat_seq to the data, using parametric empirical Bayesian adjustment
-#combat_edata <- ComBat(dat=counts2, batch=batch, 
-#					   mod=modcombat, par.prior=TRUE)
-combat_edata <- ComBat_seq(counts=as.matrix(counts3[,-c(1)]), batch=batch, covar_mod=modcombat)
+combat_edata <- ComBat_seq(counts=as.matrix(counts3[,-c(1)]), batch=batch)
 
 # Append gene names back 
 combat <- as.data.frame(combat_edata)
