79be54b0151173728d668de553ceb53c94deb0dd
Mollie Peters <mjpete11@asu.edu>
2022-11-05 15:30:24 -0700

    tried a different function to perform quantile normalization

--- linear_models/normalization/preprocessCore_quantile_normalization.R
+++ linear_models/normalization/preprocessCore_quantile_normalization.R
@@ -4,24 +4,13 @@
 library(data.table)
 library(tidyverse)
 library(reshape)
-library(sva)
 library(preprocessCore)
 
 #_______________________________________________________________________________ 
-# Make matrix of genes, samples, counts, organs, and batch effects
+# Read in combat_seq batch adjusted matrix 
 #_______________________________________________________________________________ 
-# Read in GTEx manifest
-manifest <- read.csv("/scratch/mjpete11/linear_models/data/sample.tsv", header=TRUE, sep = "\t")
-
-# Make dataframe with sample id, tissue type
-# All of the rin number and ischemic time values were missing...
-df1 <- data.frame(manifest$"dbgap_sample_id", manifest$"tissue_type")
-
-# Remove all rows with NA in either columns
-df2 <- df1[complete.cases(df1), ]
-
 # Read in GTEx counts
-counts <- fread("/scratch/mjpete11/linear_models/data/combat_batch_adjusted_counts.csv", sep=",")
+counts <- fread("/scratch/mjpete11/linear_models/data/combat_seq_filtered.csv", sep=",")
 
 # Drop the index column
 counts$V1 <- NULL
@@ -30,20 +19,22 @@ counts$V1 <- NULL
 # Perform quantile normalization 
 #_______________________________________________________________________________ 
 #perform quantile normalization
-df_norm <- as.data.frame(normalize.quantiles(as.matrix(counts)))
+mat  <- as.matrix(counts[,2:ncol(counts)])
+df_norm <- as.data.frame(normalize.quantiles(mat))
 
 df_norm[1:5,1:5]
-counts[1:5,1:5]
+counts[1:5,2:6]
 
-dim(df_norm)==dim(counts) # TRUE TRUE
+dim(df_norm)==dim(counts[,2:ncol(counts)]) # TRUE TRUE
 
 # Add sample names column
-colnames(df_norm) <- colnames(counts)
+colnames(df_norm) <- colnames(counts[,2:ncol(counts)])
 
 # Add gene name column
-gene_names <- fread("/scratch/mjpete11/linear_models/data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct", sep="\t",
-					   	select=c("Description"))
-df_norm$"Description" <- gene_names 
+gene_names <- counts$"Description" 
+
+# Add gene name column 
+df_norm$"Description" <- gene_names
 
 # Move the gene name column to the front
 df_norm2 <- df_norm %>% select("Description", everything())
