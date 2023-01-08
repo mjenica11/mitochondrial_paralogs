8bca9fd183c8232b185095da98cf44a715c02e2a
Mollie Peters <mjpete11@asu.edu>
2023-01-08 15:24:24 -0700

    moved filtering step to separate script from combat_seq.R

---
+++ linear_models/normalization/filter_counts.R
@@ -0,0 +1,63 @@
+# Perform combat normalization on count data
+
+# Libraries
+library(data.table)
+library(tidyverse)
+library(reshape)
+library(sva)
+library(dplyr)
+
+# Read in GTEx counts
+counts <- fread("/scratch/mjpete11/linear_models/data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct", sep="\t")
+
+# Read in metadata with batch variables
+manifest <- read.csv("/scratch/mjpete11/linear_models/data/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt", header=TRUE, sep = "\t")
+
+# Drop samples from manifest that are missing in the count data
+rownames(manifest) <- manifest$SAMPID
+manifest <- manifest[(manifest$SAMPID %in% colnames(counts)[3:ncol(counts)]),]
+
+# ComBat-seq does not support 1 sample per batch
+# Remove samples (rows) that were sequenced alone from the manifest
+mani <- manifest[manifest$SMGEBTCH %in% manifest$SMGEBTCH[duplicated(manifest$SMGEBTCH)],]
+
+# Only keep samples that are present in the manifest which were sequenced 
+# with at least one other sample
+samples <- mani$SAMPID
+counts2 <- counts[,..samples]
+
+# Append gene names back 
+counts2$"Description" <- counts$"Description"
+
+# Move the gene names column to the front
+counts2 <- counts2 %>% select("Description", everything())
+
+# Expression filter: Keep a gene if it has >5 counts in at least 1 samples 
+expression_filter <- function(DF, thresh){
+				DAT <- DF[rowSums(DF>thresh)>=1, ]
+				return(DAT)
+}
+
+# Apply function
+counts3 <- expression_filter(DF=counts2, thresh=5)
+
+# Write filtered counts to file
+counts3 <- write.csv("/scratch/mjpete11/linear_models/data/filtered_counts.csv")
+
+# Did any SLCs drop out?
+SLC <- c("SLC25A1", "SLC25A2", "SLC25A3", "SLC25A4", "SLC25A5", "SLC25A6",
+		 "UCP1", "UCP2", "UCP3", "SLC25A10", "SLC25A11", "SLC25A12", 
+		 "SLC25A13", "SLC25A14", "SLC25A15", "SLC25A16", "SLC25A17", 
+		 "SLC25A18", "SLC25A19", "SLC25A20", "SLC25A21", "SLC25A22", 
+		 "SLC25A23", "SLC25A24", "SLC25A25", "SLC25A26", "SLC25A27", 
+		 "SLC25A28", "SLC25A29", "SLC25A30", "SLC25A31", "SLC25A32", 
+		 "SLC25A33", "SLC25A34", "SLC25A35", "SLC25A36", "SLC25A37", 
+		 "SLC25A38", "SLC25A39", "SLC25A40", "SLC25A41", "SLC25A42", 
+		 "SLC25A43", "SLC25A44", "SLC25A45", "SLC25A46", "SLC25A47",
+		 "SLC25A48", "MTCH1", "MTCH2", "SLC25A51", "SLC25A52", "SLC25A53")
+
+# Are any genes missing?
+setdiff(SLC, counts3$'Description')  # No
+
+# How many genes are left?
+nrow(counts3) # 56200 
