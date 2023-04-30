a94d18217c3fdef5a9d44ff3e48e880e1ebed741
Mollie Peters <mjpete11@asu.edu>
2023-04-30 15:49:55 -0700

    fixed an error in the filtering function

--- linear_models/normalization/filter_counts.R
+++ linear_models/normalization/filter_counts.R
@@ -10,6 +10,8 @@ library(dplyr)
 # Read in GTEx counts
 counts <- fread("/scratch/mjpete11/linear_models/data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct", sep="\t")
 
+dim(counts) # 56200 17384
+
 # Read in metadata with batch variables
 manifest <- read.csv("/scratch/mjpete11/linear_models/data/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt", header=TRUE, sep = "\t")
 
@@ -31,15 +33,47 @@ counts2$"Description" <- counts$"Description"
 
 # Move the gene names column to the front
 counts2 <- counts2 %>% select("Description", everything())
+dim(counts2) # 56200 17312
 
-# Expression filter: Keep a gene if it has >5 counts in at least 1 samples 
+# Expression filter: Keep a gene if it has >x counts in at least 1 sample 
+# Skip the first column because that is just the gene name column
 expression_filter <- function(DF, thresh){
-				DAT <- DF[rowSums(DF>thresh)>=1, ]
+				DAT <- DF[rowSums(DF[,2:ncol(DF)]>=thresh, na.rm=TRUE)>0, ]
 				return(DAT)
 }
 
 # Apply function
 counts3 <- expression_filter(DF=counts2, thresh=5)
+dim(counts3) # 51259 17312 
+
+# Use another function to test if it is working...
+tmp <- counts2[rowSums(counts2[,2:ncol(counts2)]>=5, na.rm=TRUE)>0,] 
+dim(tmp) # 51259 17312
+
+################################ test code ####################################
+# Create a sample dataframe
+dat <- data.frame(a = c("X", "Y", "Z", "W"),
+				 b = c(1, 2, 3, 4),
+				 c = c(5, 4, 3, 2),
+				 d = c(9, NA, 11, 12))
+
+# Drop rows with no value greater than or equal to 5 in columns b, c, and d
+dat <- dat[rowSums(dat[, 2:ncol(dat)] >= 5, na.rm = TRUE) > 0, ]
+dim(dat) #  3 4
+# Drop rows with no value greater than or equal to 5
+dat1 <- dat[rowSums(dat[, 2:ncol(dat)] >= 5, na.rm = TRUE) > 0, ]
+dim(dat1) #  3 4
+dat2 <- expression_filter(DF=dat, thresh=5)
+dim(dat2) # 3 4
+#...Both functions are definitely working...
+# Diagnosed the problem! Needed to explicitely skip the gene name column
+################################ test code ####################################
+
+# How many genes are left if you set the threshold to 10?
+counts_10 <- expression_filter(DF=counts2, thresh=10)
+dim(counts_10) # 47465 17312
+test0 <- counts2[rowSums(counts2[,2:ncol(counts2)]>=10, na.rm=TRUE)>0,] # Still got the same result
+dim(test0) # 47465 17313.. results can be replicated
 
 # Write filtered counts to file
 write.csv(counts3, "/scratch/mjpete11/linear_models/data/filtered_counts.csv")
@@ -58,6 +92,3 @@ SLC <- c("SLC25A1", "SLC25A2", "SLC25A3", "SLC25A4", "SLC25A5", "SLC25A6",
 
 # Are any genes missing?
 setdiff(SLC, counts3$'Description')  # No
-
-# How many genes are left?
-nrow(counts3) # 56200 
