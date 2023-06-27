15c05d50b6ea971a33af67e09fee273331cca499
Mollie Peters <mjpete11@asu.edu>
2023-06-27 15:09:11 -0700

    MaREA startup code

---
+++ linear_models/marea/marea.m
@@ -0,0 +1,6 @@
+% Code to start up MaREA to perform metabolic reaction enrichment analysis
+
+addpath("/scratch/mjpete11/linear_models/marea/MaREA")
+addpath("/scratch/mjpete11/linear_models/marea/cobratoolbox")
+
+MaREA
