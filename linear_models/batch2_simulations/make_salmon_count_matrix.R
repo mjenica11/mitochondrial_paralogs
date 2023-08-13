154f071026d83a3f944b3288dc59f19253a1e0f9
Mollie Peters <mjpete11@asu.edu>
2023-08-12 18:35:45 -0700

    updated with new batch2 counts

--- linear_models/batch2_simulations/make_salmon_count_matrix.R
+++ linear_models/batch2_simulations/make_salmon_count_matrix.R
@@ -8,20 +8,22 @@ library(GenomicFeatures)
 library(AnnotationDbi)
 library(edgeR)   
 library(readr)
+library(dplyr)
+library(data.table)
 
 # Input
 #METADATA = "/scratch/mjpete11/linear_models/data/filtered_counts_organ_metadata.csv"
-
-# Read Metadata CSV.                                                            
-#Samples = read.csv(METADATA, header = TRUE)
 
 # Output
 GENE_FILE = "/scratch/mjpete11/linear_models/data/simulated_batch2_salmon_count_matrix_1000_samples.tsv"
 #TEST = "/scratch/mjpete11/linear_models/data/simulated_data_salmon_count_matrix_20_samples.tsv"
 
+# Read Metadata CSV.                                                            
+#Samples = read.csv(METADATA, header = TRUE)
+
 # Create a vector of sample names
 numbers <- seq(1, 1000)
-for (i in 1:10){
+for (i in 1:1000){
 		if (i <= 9) {
 				numbers[i] <- paste0("0",i)
 		} else {
@@ -54,40 +56,38 @@ head(tx2gene)
 Txi_Gene <- tximport(files, type = "salmon", tx2gene = tx2gene, txOut = FALSE, ignoreAfterBar=TRUE)      
 Txi_Gene$counts[1:5,1:5]
 Counts_Gene <- Txi_Gene$counts
-dim(Counts_Gene) # 61386 1000
+dim(Counts_Gene)
 df_Gene <- data.frame(Counts_Gene, check.names = TRUE)
-dim(df_Gene)
-head(df_Gene)
+dim(df_Gene) # 61386 1000
 
 # Name the gene ID column 
-#df_Gene$gene <- rownames(df_Gene)
-library(tibble)
-df_Gene <- tibble::rownames_to_column(df_Gene, "gene") 
-head(df_Gene$gene)
-df_Gene[1:5,1:5]
-dim(df_Gene)
+#df_Gene$gene <- row.names(df_Gene)
+df_Gene1 <- df_Gene
+df_Gene1 <- tibble::rownames_to_column(df_Gene1, "ensembl_ID")
+df_Gene1[1:5,1:5]
+df_Gene1 <- df_Gene1 %>% relocate(gene) %>% head()
+dim(df_Gene1)
 
 # Make a smaller test dataframe and save rownames as a column so it can be 
 # read back in correctly
 rm(test)
-df_Gene$row_name <- row.names(df_Gene)
-test <- df_Gene[1:20,1:20]
+df_Gene1$row_name <- row.names(df_Gene1)
+test <- df_Gene1[1:20,1:20]
 test[1:5,1:5]
-dim(df_Gene)
+dim(df_Gene1) # 61386 1001
 
 object.size("sample_1000") # 120 bytes
 object.size("ENSG00000000003.15") # 136 bytes
-sizes <- sapply(colnames(df_Gene), object.size)
+sizes <- sapply(colnames(df_Gene1), object.size)
 median(sizes) # 120 bytes
-sizes_y <- sapply(rownames(df_Gene), object.size)
+sizes_y <- sapply(rownames(df_Gene1), object.size)
 median(sizes_y) # 136 bytes
 
 # Write to TSV
-write.table(df_Gene, file = GENE_FILE, sep = "\t", row.names = TRUE, quote = FALSE)
-#write.table(df_Gene, file = TEST, sep = "\t", row.names = TRUE, quote = FALSE)
+write.table(df_Gene1, file = GENE_FILE, sep = "\t", row.names = TRUE, quote = FALSE)
+#write.table(df_Gene1, file = TEST, sep = "\t", row.names = TRUE, quote = FALSE)
 
 # Try reading the table back in
-library(data.table)
 rm(dat)
 dat <- fread("/scratch/mjpete11/linear_models/data/simulated_data_salmon_count_matrix_20_samples.tsv", sep="\t")
 dat[1:5,1:5]
