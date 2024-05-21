5042d778ae6e4a66b415569791b7b83243c334df
[ Mollie Peters - Sols Graduate Programs - Graduate Student - mjpete11 ] <mjpete11@c035.sol.rc.asu.edu>
2024-05-20 17:37:18 -0700

    layered histograms of SLC25s and biomarkers in heart and liver

--- linear_models/exploratory/histograms.R
+++ linear_models/exploratory/histograms.R
@@ -18,7 +18,9 @@ library(scales)
 library(edgeR)
 
 # Read in combat_seq normalized counts 
-counts <- fread("/scratch/mjpete11/linear_models/data/combat_seq_filtered.csv", sep=",")
+#counts <- fread("/scratch/mjpete11/mitochondrial_paralogs/linear_models/data/data/combat_seq_filtered.csv", sep=",")
+counts <- fread("/scratch/mjpete11/mitochondrial_paralogs/linear_models/data/data/voom_quantile_normalized_counts.csv", sep=",")
+counts[1:5,1:5]
 
 # Drop the index column
 counts$V1 <- NULL
@@ -38,22 +40,29 @@ SLC <- c("SLC25A1", "SLC25A2", "SLC25A3", "SLC25A4", "SLC25A5", "SLC25A6",
 		 "SLC25A43", "SLC25A44", "SLC25A45", "SLC25A46", "SLC25A47",
 		 "SLC25A48", "MTCH1", "MTCH2", "SLC25A51", "SLC25A52", "SLC25A53")
 
+# List of metabolic biomarkers 
+biomarkers <- c("SLC16A1", "SLC16A2", "HK1", "HK2", "HK3", "GPI", "PFKM","PFKX", "PFKP", "ALDOA", "ALDOB", "TPI1", "GAPDH", "PGK1","PGK2", "PGAM1", "PGAM5", "PGAM2", "PGAM4", "ENO1","PKLR", "LDHA", "LDHB", "LDHC", "LDHD", "CACT", "CPT1A","CPT1B", "CPT2", "ACADVL", "ACADL", "ACADM", "ACADS","ACADSB", "ACAD11", "ACAD8", "ACAD10", "ACAD9", "ECHS1","ECH1", "ECI1", "ECI2", "ECHS1", "HADHA", "PDHA1", "PDHA2","PDHB", "PDP1", "PDHX", "CS", "ACO1", "ACO2", "IDH3G","IDH1", "IDH2", "IDH3A", "IDH3B", "OGDH", "SUCLG2","SUCLG1", "SUCLA2", "SDHB", "SDHD", "SDHA", "SDHC", "SDHAF2","FH", "MDH1", "MDH2", "MT-ND1")
+
+genes <- c(SLC, biomarkers)
+genes
+
 # Subset the SLC25 genes
 counts <- as.data.frame(counts)
-sub_df <- counts[counts$"Description" %in% SLC, ]
+sub_df <- counts[counts$"Description" %in% genes, ]
 
 # Write to file
 #write.table(sub_df, "/scratch/mjpete11/linear_models/data/SLC_df_voom_combat_seq.csv", sep=",")
 
 # Read in count df of just SLC genes
-sub_df <- read.csv("/scratch/mjpete11/linear_models/data/SLC_df_voom_combat_seq.csv", sep=",")
-names(sub_df) <- gsub(x=names(sub_df), pattern="\\.", replacement="-")
+#sub_df <- read.csv("/scratch/mjpete11/mitochondrial_paralogs/linear_models/data/data/SLC_df_voom_combat_seq.csv", sep=",")
+#names(sub_df) <- gsub(x=names(sub_df), pattern="\\.", replacement="-")
 
 # All present!
-setdiff(SLC, sub_df$'Description') 
+#setdiff(SLC, sub_df$'Description') 
+setdiff(genes, sub_df$'Description')  # PFKX PGAM2 CACT
 
 # Read in sample attributes files
-file <- read.csv("/scratch/mjpete11/linear_models/data/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt", sep="\t")
+file <- read.csv("/scratch/mjpete11/mitochondrial_paralogs/linear_models/data/data/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt", sep="\t")
 
 # Subset heart left ventricle and liver samples into two separate dfs
 heart <- file[file$SMTSD %in% "Heart - Left Ventricle", ]
@@ -119,17 +128,19 @@ length(unique(organs$SUBJID)) # 508 individuals
 any(duplicated(organs)) # FALSE
 
 # Change the name of 'value' column to 'combat_seq_counts'
-colnames(organs)[4] <- "combat_seq_counts"
+#colnames(organs)[4] <- "combat_seq_counts"
+colnames(organs)[4] <- "voom_qnorm_counts"
 
 # Convert combat_seq adjusted counts to log2 and CPM
 # Values of zero will be set to 2
-organs$log2_cpm <- cpm(organs$combat_seq_counts, log=TRUE, prior.count=2)
+#organs$log2_cpm <- cpm(organs$combat_seq_counts, log=TRUE, prior.count=2)
+organs$log2_cpm <- cpm(organs$voom_qnorm_counts, log=TRUE, prior.count=2)
 
 # Write organs df to file
 #write.table(organs, "/scratch/mjpete11/linear_models/data/organs_combat_seq_all_heart_liver_samples.csv", sep=",")
 
 # Set y lim
-range(organs$combat_seq_counts) # c(0 265290) 
+#range(organs$combat_seq_counts) # c(0 265290) 
 range(organs$log2_cpm) # c(-5.96, 11.06) 
 
 # Why does SLC25A47 look like it only has zero values? 
@@ -144,16 +155,19 @@ rm(histogram_plots)
 histogram_plots <- function(GENE){
 	dat <- organs %>% filter(gene == GENE)
     # Violin plot
-	p <- ggplot(dat, aes(x = combat_seq_counts, fill=organ)) +
+	#p <- ggplot(dat, aes(x = combat_seq_counts, fill=organ)) +
+	p <- ggplot(dat, aes(x = voom_qnorm_counts, fill=organ)) +
 	geom_histogram(bins=100) +
 	scale_color_manual(values = c("green", "purple")) +
 #	geom_bin2d(bins = 100) +
 	#ylim(c(0,30)) +
 	ylab("frequency") +
-	xlab("filtered and combat_seq adjusted counts") +
+#	xlab("filtered and combat_seq adjusted counts") +
+	xlab("filtered, quantile adjusted, and voom adjusted counts") +
 	theme(plot.title = element_textbox_simple(size=10)) + # automatically wrap long titles
 	ggtitle(paste0("Histogram plot of ", GENE, " expression in heart and liver")) 
-	ggsave(paste0("/scratch/mjpete11/linear_models/exploratory/plots/", GENE, ".png"), p, device="png")
+	ggsave(paste0("/scratch/mjpete11/mitochondrial_paralogs/linear_models/data/data/histogram_plots/", GENE, ".png"), p, device="png")
 }
-plots <- Map(histogram_plots, GENE=SLC)
-
+plot.new()
+plots <- Map(histogram_plots, GENE=genes)
+dev.off()
